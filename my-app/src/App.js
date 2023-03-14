91c219791e1ff2acb8209b7d7bb8a9e6b18c1ddf
fahiid33 <trey34809@gmail.com>
2023-03-14 03:15:52 +0100

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo192.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          First interaction with this shit
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/fahiid33"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Owner
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
